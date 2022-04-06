1125ecd36d20bae2d5b424afa59640ca6f5d8d3e
changmin <89798038+cm5123@users.noreply.github.com>
2022-04-06 17:08:06 +0900

    Delete 뇌졸중 예측앱 작성 프로젝트 directory

--- 뇌졸중 예측앱 작성 프로젝트/flask_app/db_data.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import sqlite3
-
-df = pd.read_csv('data1.csv', encoding='cp949')
-
-con = sqlite3.connect("df.db")
-
-df.to_sql("df", con, if_exists="append", index=False)
-con.commit()
